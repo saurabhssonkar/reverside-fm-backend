d51484121a6aa0f9ba8006ad0ff41cc4f30b7644
saurabhssonkar <59632111+saurabhssonkar@users.noreply.github.com>
2025-05-16 11:03:16 +0530

    initail setup for the webrtc sfu  multi connection

---
+++ backend/aws/s3.js
@@ -0,0 +1,10 @@
+// aws/s3.js
+const AWS = require('aws-sdk');
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: 'us-east-1', // Replace with your actual region
+});
+
+module.exports = s3;
